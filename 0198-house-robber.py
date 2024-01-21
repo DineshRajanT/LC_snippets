69f0aa9a7f4c6122519f381c1bc9a21ad3ba03c3
ryanxjacques <ryanxjacques@gmail.com>
2024-01-21 14:32:17 -0800

    Time: 34 ms (83.40%), Space: 16.58 (57.82%)

---
+++ 0198-house-robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # T: O(n)
+        # S: O(1)
+
+        rob1, rob2 = 0, 0 # rob1 is n-1, rob2 is n-2
+
+        for n in nums:
+            temp = max(rob2 + n, rob1)
+            rob2 = rob1
+            rob1 = temp
+            
+        return rob1
