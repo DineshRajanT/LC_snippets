2d672aaefe192188253b93f3312b51c7356e790b
DINESHRAJAN T <37593528+DineshRajanT@users.noreply.github.com>
2024-01-22 09:50:14 +0530

    Update 645_DT_Set_Mismatch.py

--- 645_DT_Set_Mismatch.py
+++ 645_DT_Set_Mismatch.py
@@ -6,16 +6,12 @@ class Solution:
     
         for val in nums:
             counts[val] = counts.get(val,0)+1
-            if counts[val] > 1:
-                dups = val
-            if counts[val] == 0:
-                missing = val
 
-        # for k,v in counts.items():
-        #     if v > 1:
-        #         dups = k
-        #     if v == 0:
-        #         missing = k
+        for k,v in counts.items():
+            if v > 1:
+                dups = k
+            if v == 0:
+                missing = k
                 
         return [dups, missing]
 
