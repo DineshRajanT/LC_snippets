8147c4167f5bef41a9d393bc19cf6558df847b9d
ryanxjacques <ryanxjacques@gmail.com>
2024-01-22 03:55:40 -0800

    Added solution for 12/23, updated file nameing for convention

---
+++ 1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters.py
@@ -0,0 +1,28 @@
+from collections import Counter
+from typing import List, Set
+
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+
+        def backtracking(i: int, charSet: Set[str]) -> int:
+            if i == len(arr):
+                return len(charSet)
+            
+            newString = arr[i]
+            maxLength = 0
+
+            # Option 1: Include the new string (if it's unique)
+            if isUnique(charSet, newString):
+                    newSet = set(charSet)
+                    for c in newString:
+                        newSet.add(c)
+                    maxLength = backtracking(i + 1, newSet)
+        
+            # Option 2: Don't include the new string
+            return max(maxLength, backtracking(i + 1, charSet))
+        
+        def isUnique(charSet: Set[str], newString: str) -> bool:
+            count = Counter(charSet) + Counter(newString)
+            return max(count.values()) == 1
+    
+        return backtracking(0, set())
